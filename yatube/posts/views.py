c4482132738de81a7dbbef82a66b57e8ee78675e
Надежда Бессмертная <nadezhda.bessmertnaya@gmail.com>
2021-12-07 22:45:25 +0300

    Исправлена ошибка в моделях

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 from .models import Post, Group
 
 def index(request):
